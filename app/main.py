9367f57267b3dfc3a578ac81dadbfff8b01ddf56
Sachin Khanal <sachinkhanal412@gmail.com>
2026-03-19 17:31:50 -0400

    feat: Initialize FastAPI application with CORS and relocate schema documentation to `app/models`.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
 from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Sports Analysis App API",
+    description="API for analyzing sports data",
+    version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://frontend-bc7.pages.dev/" # production frontend
+        "http://localhost:5173/" # Vite local server
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Sports Analysis App API is running..."}
+
+if __name__ == "__main__":
+    import uvicorn
+    from app.config import PORT
+
+    uvicorn.run("app.main:app", host="[IP_ADDRESS]", port=PORT, reload=True)
